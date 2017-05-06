b68081466f0e5a0f323b482029c3a7198830e015
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2017-05-06 18:51:11 -0300

    194: Modificado o nome do pacote basico para ezl.

---
+++ ezl/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for easyLawyer project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "easylawyer.settings")
+
+application = get_wsgi_application()
