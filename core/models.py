5eb3502cf9b491c678a186ec2fa9d1296b51066a
Luís Antônio Martins dos Reis <luis.martins.reis@outlook.com>
2017-05-08 20:23:50 -0300

    [176]:Cria??o da classe TypeMovement, altera??o no arquivo de configura??o para inclus?o do App lawsuit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Audit(models.Model):
+    create_date = models.DateTimeField()
+    alter_date = models.DateTimeField(blank=True, null=True)
+    create_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+                                       related_name='%(class)s_create_user')
+    alter_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, blank=True, null=True,
+                                      related_name='%(class)s_alter_user')
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
