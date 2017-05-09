65fb5ce4c46f13ff41fc35619e54ea89f008c5b6
Luís Antônio Martins dos Reis <luis.martins.reis@outlook.com>
2017-05-08 21:32:34 -0300

    [176]:Cria??o das classes BaseForm, TypeMovementForm, inclus?o dos arquivos forms.py, urls.py na app lawsuit

---
+++ lawsuit/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.forms import ModelForm
+from .models import TypeMovement
+
+
+# Cria uma Form referência e adiciona o mesmo style a todos os widgets
+class BaseForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(BaseForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control input-sm'
+
+
+class TypeMovementForm(BaseForm,forms.Form):
+    class Meta:
+        model = TypeMovement
+        fields = ['name','legacy_code','uses_wo']
+
+    name = forms.CharField(
+        label=u"Nome",
+        max_length=255,
+        required=True,
+        widget=forms.TextInput()
+    )
+
+    legacy_code = forms.CharField(
+        label=u"Código Legado",
+        max_length=255,
+    )
+
+    use_wo = forms.BooleanField(
+        label=u"Utiliza wo?",  # FIXME verificar o significado de 'wo'
+        required=True,
+        widget=forms.CheckboxInput
+    )
