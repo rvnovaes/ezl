25ae5fef4ca0858da6621fb80111faeb2ec34952
lfmrodrigues36 <dggccgn@hotmail.com>
2017-05-12 16:31:24 -0300

    [188]: Caso iniciado

--- lawsuit/models.py
+++ lawsuit/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from core.models import Audit
 # Create your models here.
 
-
 class TypeMovement(Audit):
     name = models.CharField(max_length=255,blank=False,null=False,default="",unique=True)
     legacy_code = models.CharField(max_length=255,blank=False,null=False,default="",unique=True)
