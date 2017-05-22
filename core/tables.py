d2eb8edfb1dd8ca52f40666b85435e5f9e401c20
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2017-05-21 21:58:37 -0300

    Modificada a coluna de nome para coluna tipo link que aponta para a edi??o do registro.

--- core/tables.py
+++ core/tables.py
@@ -1,12 +1,14 @@
 import django_tables2 as tables
+from django_tables2.utils import A  # alias for Accessor
 
 from .models import Person
 
 
 class PersonTable(tables.Table):
+    name = tables.LinkColumn(viewname='person_update', attrs={'a': {'target': '_blank'}}, args=[A('pk')])
+
     class Meta:
         model = Person
-        fields = ['legal_name', 'name', 'is_lawyer', 'is_corresponding', 'legal_type', 'auth_user', 'active',
-                  'cpf_cnpj']
+        fields = ['name', 'is_lawyer', 'is_corresponding', 'legal_type', 'auth_user', 'active', 'cpf_cnpj']
         attrs = {"class": "table-striped table-bordered"}
         empty_text = "Não existem pessoas cadastradas"
