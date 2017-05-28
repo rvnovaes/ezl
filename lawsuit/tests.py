5a2e2491c5ed038289c3f1eeb7c0977093e5f351
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2017-05-28 12:18:19 -0300

    Colocado esboco de teste em TypeMovement

--- lawsuit/tests.py
+++ lawsuit/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import TypeMovement
 
-# Create your tests here.
+
+class TypeMovementTestCase(TestCase):
+
+    def setUp(self):
+        TypeMovement.objects.create(name='Audiência', legacy_code='11.02', uses_wo=True)
+
+    def test_type_movement_uses_wo(self):
+        type_movement = TypeMovement.objects.get(name='Audiência')
+        self.assertTrue(type_movement.uses_wo)
