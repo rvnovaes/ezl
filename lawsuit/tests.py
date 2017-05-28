f2a05f4560a691a0db9af235cd9a45ccbe03b836
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2017-05-28 12:36:47 -0300

    Terminado exemplo do primeiro teste simples.

--- lawsuit/tests.py
+++ lawsuit/tests.py
@@ -1,3 +1,5 @@
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 from django.test import TestCase
 from .models import TypeMovement
 
@@ -5,7 +7,8 @@ from .models import TypeMovement
 class TypeMovementTestCase(TestCase):
 
     def setUp(self):
-        TypeMovement.objects.create(name='Audiência', legacy_code='11.02', uses_wo=True)
+        self.user = User.objects.create_user(username='jacob', email='jacob@gmail.com', password='top_secret')
+        TypeMovement.objects.create(create_user=self.user, create_date=now(), name='Audiência', legacy_code='11.02', uses_wo=True)
 
     def test_type_movement_uses_wo(self):
         type_movement = TypeMovement.objects.get(name='Audiência')
