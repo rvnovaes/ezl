627fc97aa5e229aefdb7c03c23f4f0b697543650
Luís Antônio Martins dos Reis <luis.martins.reis@outlook.com>
2017-06-30 15:06:32 -0300

    [184]: mudanca do widget datetimerangefilter para exibir e filtrar por dia,mes e ano.

--- ezl/settings.py
+++ ezl/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = ''
+# STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
