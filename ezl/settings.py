2eead9746aeaf2cd201590d7f205fd4e54b1dbf1
Luís Antônio Martins dos Reis <luis.martins.reis@outlook.com>
2017-06-29 21:28:37 -0300

    Correcao de configuracao de arquivos estaticos

--- ezl/settings.py
+++ ezl/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f5*(8sgk)n1!i52xijv0yt@jtewp28%g%sp1rx*=y68ocgg+!2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -143,17 +143,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
+# if DEBUG:
+#
+#     STATIC_ROOT = ''
+#
+#     STATICFILES_DIRS = (
+#         os.path.join(BASE_DIR, "static/"),
+#     )
+#
+# else:
+#
+#     STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-    STATIC_ROOT = ''
-
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, "static/"),
-    )
-
-else:
-
-    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
--- ezl/urls.py
+++ ezl/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -21,7 +23,7 @@ urlpatterns = [
                       login_required(ClientAutocomplete.as_view()),
                       name='client_autocomplete'),
 
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(
+              ] + static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, "static")) + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
