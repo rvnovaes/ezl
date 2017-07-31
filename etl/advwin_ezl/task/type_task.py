ef0f33e4a1cdd5c1b5dd0d53fe9b792e071f0fd6
Luís Antônio Martins dos Reis <luis.martins.reis@outlook.com>
2017-07-31 17:14:29 -0300

    Atualizacao do ETL de Tipo de servico para buscar o id dos formularios do ADVwin

--- etl/advwin_ezl/task/type_task.py
+++ etl/advwin_ezl/task/type_task.py
@@ -1,11 +1,15 @@
 from core.utils import LegacySystem
 from etl.advwin_ezl.advwin_ezl import GenericETL
-from task.models import TypeTask
+from task.models import TypeTask, SurveyType
+
+survey_dict = {'audienciaCorrespondente': 'COURTHEARING',
+               'diligenciaCorrespondente': 'DILIGENCE', 'protocoloCorrespondente': 'PROTOCOL',
+               'alvaraCorrespondente': 'OPERATIONLICENSE'}
 
 
 class TypeTaskETL(GenericETL):
     model = TypeTask
-    query = "SELECT  tm.Codigo,  tm.Descricao FROM Jurid_CodMov AS tm  " \
+    query = "SELECT  tm.Codigo,  tm.Descricao, tm.formulario_id FROM Jurid_CodMov AS tm  " \
             "WHERE right(tm.Codigo, 1) <> '.'  " \
             "   AND (tm.UsarOS = 1 AND tm.UsarOS IS NOT NULL)  " \
             "   AND tm.Status = 'Ativo'  " \
@@ -18,19 +22,24 @@ class TypeTaskETL(GenericETL):
         for row in rows:
             code = row['Codigo']
             name = row['Descricao']
+            survey_key = row['formulario_id']
+            key = survey_dict[survey_key]
+            survey_type = SurveyType[key].name.title()
 
             # tem que verificar se é novo antes para não salvar o create_user ao fazer update
             instance = self.model.objects.filter(legacy_code=code,
                                                  system_prefix=LegacySystem.ADVWIN.value).first()
             if instance:
                 instance.name = name
+                instance.survey_type = survey_type
                 instance.alter_user = user
                 instance.is_active = True
-                instance.save(update_fields=['is_active', 'name', 'alter_user', 'alter_date'])
+                instance.save(update_fields=['is_active', 'name', 'alter_user', 'alter_date', 'survey_type'])
             else:
                 self.model.objects.create(name=name,
                                           is_active=True,
                                           legacy_code=code,
+                                          survey_type=survey_type,
                                           system_prefix=LegacySystem.ADVWIN.value,
                                           create_user=user,
                                           alter_user=user)
