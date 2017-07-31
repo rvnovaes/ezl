0c71330ae692d65c82d4b853c0fe0680a4e0c13c
Guilherme Nascimento <gnascimento@localhost>
2017-07-31 01:36:26 -0300

    Caso 267: Re-Push

--- ezl/settings.py
+++ ezl/settings.py
@@ -98,9 +98,12 @@ WSGI_APPLICATION = 'ezl.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gsnasc',
-        'USER': 'gsnasc',
-        'PASSWORD': 'nacigi-2'
+        'NAME': 'ezl_datatest',
+        'USER': 'ezl',
+        'PASSWORD': 'ezl',
+        'HOST': '13.68.213.60',
+        # 'HOST': '127.0.0.1',
+        'PORT': '5432'
 
     }
 }
