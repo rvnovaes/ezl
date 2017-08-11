4ae4518f065a90662ab17c2b3b35575947a3f1f3
Luiz Fernando Moreira Rodrigues <dggccgn@gmail.com>
2017-08-11 12:00:04 -0300

    [294]: Exibir hist?rico das tasks no detalhe da provid?ncia

--- task/views.py
+++ task/views.py
@@ -20,7 +20,7 @@ from core.views import BaseCustomView, MultiDeleteViewMixin, SingleTableViewMixi
 from task.signals import send_notes_execution_date
 from .filters import TaskFilter
 from .forms import TaskForm, TaskDetailForm, TypeTaskForm
-from .models import Task, TaskStatus, Ecm, TypeTask
+from .models import Task, TaskStatus, Ecm, TypeTask, TaskHistory
 from .tables import TaskTable, DashboardStatusTable, TypeTaskTable
 
 
@@ -167,13 +167,12 @@ class TaskDetailView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super(TaskDetailView, self).get_context_data(**kwargs)
         context['geds'] = Ecm.objects.filter(task_id=self.object.id)
+        context['task_history'] = TaskHistory.objects.filter(task_id=self.object.id)
         return context
 
 
 class EcmCreateView(LoginRequiredMixin, CreateView):
-
     def post(self, request, *args, **kwargs):
-
 
         files = request.FILES.getlist('path')
         task = kwargs['pk']
@@ -182,9 +181,8 @@ class EcmCreateView(LoginRequiredMixin, CreateView):
 
         data = {'success': False,
                 'message': exception_create()
-                        }
+                }
         for file in files:
-
 
             ecm = Ecm(path=file,
 
@@ -192,7 +190,6 @@ class EcmCreateView(LoginRequiredMixin, CreateView):
                       create_user_id=str(request.user.id),
                       create_date=timezone.now()
                       )
-
 
             try:
                 ecm.save()
@@ -384,4 +381,3 @@ class DashboardSearchView(LoginRequiredMixin, SingleTableView):
         RequestConfig(self.request, paginate={'per_page': 10}).configure(table)
         context['table'] = table
         return context
-
