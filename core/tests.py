9fd22851a8b3d497cc468871dcf1b57a03b6bc81
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2017-09-06 15:46:26 -0300

    0000404: Incluir testes unit?rios para novas funcionalidades

--- core/tests.py
+++ core/tests.py
@@ -7,82 +7,66 @@ from .forms import *
 from model_mommy import mommy
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.utils.http import urlencode
 
-    
+
 class PersonTest(TestCase):
-
     def setUp(self):
-        
         user = User.objects.create_user(username='username', password='password')
         self.client.login(username='username', password='password')
-   
+
     def test_model(self):
-        
-        #mommy deixa as coisas bem mais faaceis
-        c_inst = mommy.make(Person,name='Random')
+        # mommy deixa as coisas bem mais faaceis
+        c_inst = mommy.make(Person, name='Random')
         self.assertTrue(isinstance(c_inst, Person))
-    
-    
+
     def test_valid_PersonForm(self):
-    
         legal_type = 'F'
-        
-        data = {'legal_type': legal_type} #Unico requerido
+
+        data = {'legal_type': legal_type}  # Unico requerido
         form = PersonForm(data=data)
         print(form.errors)
-        self.assertTrue(form.is_valid())   
-      
+        self.assertTrue(form.is_valid())
+
     def test_list_view(self):
-    
-        
         url = reverse('person_list')
         resp = self.client.get(url)
 
-        self.assertEqual(resp.status_code, 200) #Se consegue alcancar a pagina
-        
-       
+        self.assertEqual(resp.status_code, 200)  # Se consegue alcancar a pagina
+
     def test_create_view(self):
-    
-        
         url = reverse('person_add')
         resp = self.client.get(url)
-        
+
         self.assertEqual(resp.status_code, 200)
-    
-       
+
     def test_update_view(self):
-    
-        c_inst = mommy.make(Person,legal_type='F')
-        url = reverse('person_update',kwargs={'pk':c_inst.id})
+        c_inst = mommy.make(Person, legal_type='F')
+        url = reverse('person_update', kwargs={'pk': c_inst.id})
         resp = self.client.get(url)
-        
+
         self.assertEqual(resp.status_code, 200)
-        
+
     def test_delete_view(self):
-    
-        c_inst = mommy.make(Person,name='Random')
-        data = {'person_list':{c_inst.id}}
+        c_inst = mommy.make(Person, name='Random')
+        data = {'person_list': {c_inst.id}}
         url = reverse('person_delete')
-        resp = self.client.post(url,data,follow=True)
-        #print(resp.context)
-        
-        self.assertEqual(resp.status_code, 200) 
-    
+        resp = self.client.post(url, data, follow=True)
+        # print(resp.context)
+
+        self.assertEqual(resp.status_code, 200)
+
+
 class AdressTest(TestCase):
-
-
     def test_model_city(self):
-    
         c_inst = mommy.make(City)
         self.assertTrue(isinstance(c_inst, City))
-        
+
     def test_model_country(self):
-    
         c_inst = mommy.make(Country)
         self.assertTrue(isinstance(c_inst, Country))
-        
+
     def test_model_state(self):
-    
         c_inst = mommy.make(State)
         self.assertTrue(isinstance(c_inst, State))
 
@@ -104,24 +88,56 @@ class AdressTest(TestCase):
                 'state': state,
                 'city': city,
                 'address_type': address_type}
-                
+
         form = AddressForm(data=data)
         print(form.errors)
-        self.assertTrue(form.is_valid()) 
+        self.assertTrue(form.is_valid())
 
 
 class ContactMechanismTest(TestCase):
-
     def test_model(self):
-    
         c_inst = mommy.make(ContactMechanism)
         self.assertTrue(isinstance(c_inst, ContactMechanism))
 
 
 class ContactUsTest(TestCase):
+    def test_model(self):
+        c_inst = mommy.make(ContactUs)
+        self.assertTrue(isinstance(c_inst, ContactUs))
 
-     def test_model(self):
-         c_inst = mommy.make(ContactUs)
-         self.assertTrue(isinstance(c_inst, ContactUs))  
-     
 
+class AddressTypeTest(TestCase):
+    def test_model(self):
+        c_inst = mommy.make(AddressType, name='residencial')
+        self.assertTrue(c_inst, AddressType)
+
+
+class UserTest(TestCase):
+    def test_model(self):
+        c_isnt = mommy.make(User, first_name='Thiago', last_name='Rodrigues', username='thiago',
+                            email='thiago.ar17@gmail.com', password=123456)
+        self.assertTrue(c_isnt, User)
+
+    def test_valid_UserCreateForm(self):
+        data = {'first_name': 'Random',
+                'last_name': 'RandomLast',
+                'username': 'random',
+                'email': 'random@random.com',
+                'password1': 'a1b2c3d4f5',
+                'password2': 'a1b2c3d4f5'
+                }
+
+        form = UserCreateForm(data=data)
+        print(form.errors)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_senha_curta_UserCreateForm(self):
+        data = {'first_name': 'Random',
+                'last_name': 'RandomLast',
+                'username': 'random',
+                'email': 'random@random.com',
+                'password1': '12345',
+                'password2': '12345'
+                }
+        form = UserCreateForm(data=data)
+        self.assertFalse(form.is_valid())
--- task/tests.py
+++ task/tests.py
@@ -13,159 +13,140 @@ import datetime
 from django.core.files.uploadedfile import SimpleUploadedFile
 import os
 
-class TypeTaskTest(TestCase):
 
+class TypeTaskTest(TestCase):
     def setUp(self):
-        
         user = User.objects.create_user(username='username', password='password')
         self.client.login(username='username', password='password')
-
-
     def test_model(self):
-        
-        #mommy deixa as coisas bem mais faaceis
+        # mommy deixa as coisas bem mais faaceis
         c_inst = mommy.make(TypeTask)
         self.assertTrue(isinstance(c_inst, TypeTask))
-        
+
     def test_valid_TypeTaskForm(self):
-    
-        #form = TypeTaskForm()
-        #st = dict(form.fields['survey_type'].choices)['Diligence'] #Para charfield com choices em enum, usar as escolhas do enum em lowercase
-            
-        data = {'name':'Tipo1','survey_type':'Diligence'}
+        # form = TypeTaskForm()
+        # st = dict(form.fields['survey_type'].choices)['Diligence'] #Para charfield com choices em enum, usar as escolhas do enum em lowercase
+
+        data = {'name': 'Tipo1', 'survey_type': 'Diligence'}
         form = TypeTaskForm(data=data)
         print(form.errors)
-        self.assertTrue(form.is_valid())    
-        
+        self.assertTrue(form.is_valid())
+
     def test_list_view(self):
-    
         url = reverse('typetask_list')
         resp = self.client.get(url)
 
         self.assertEqual(resp.status_code, 200)
-    
+
     def test_create_view(self):
-    
         url = reverse('typetask_add')
         resp = self.client.get(url)
 
         self.assertEqual(resp.status_code, 200)
-        
+
     def test_update_view(self):
-    
         c_inst = mommy.make(TypeTask)
-        url = reverse('typetask_update',kwargs={'pk':c_inst.id})
-        resp = self.client.get(url) 
-        self.assertEqual(resp.status_code, 200)   
-    
+        url = reverse('typetask_update', kwargs={'pk': c_inst.id})
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
     def test_delete_view(self):
-    
         c_inst = mommy.make(TypeTask)
-        data = {'typetask_list':{c_inst.id}}
+        data = {'typetask_list': {c_inst.id}}
         url = reverse('typetask_delete')
-        resp = self.client.post(url,data,follow=True)
-        #print(resp.context)
-        
-        self.assertEqual(resp.status_code, 200)     
+        resp = self.client.post(url, data, follow=True)
+        # print(resp.context)
+
+        self.assertEqual(resp.status_code, 200)
+
 
 class TaskTest(TestCase):
-
     def setUp(self):
-        
         user = User.objects.create_user(username='username', password='password')
         self.client.login(username='username', password='password')
-        #self.c_inst = mommy.make(Instance,name='Random')    
+        # self.c_inst = mommy.make(Instance,name='Random')
 
     def test_model(self):
-        #mommy deixa as coisas bem mais faaceis
-        mommy.make(ContactMechanismType, name='email')
+        # mommy deixa as coisas bem mais faaceis
         c_inst = mommy.make(Task)
         self.assertTrue(isinstance(c_inst, Task))
-        
-        
+
     def test_valid_TaskForm(self):
-    
         movement = mommy.make(Movement).id
-        person_common = mommy.make(Person,name='Advogado',is_active=True, is_correspondent = False).id
-        person_correspondent = mommy.make(Person,name='Correspondente',is_active=True, is_correspondent = True).id
+        person_common = mommy.make(Person, name='Advogado', is_active=True, is_correspondent=False).id
+        person_correspondent = mommy.make(Person, name='Correspondente', is_active=True, is_correspondent=True).id
         type_task = mommy.make(TypeTask).id
         delegation_date = datetime.date.today()
-        
+
         data = {'movement': movement,
                 'person_asked_by': person_common,
                 'person_executed_by': person_correspondent,
                 'type_task': type_task,
-                'delegation_date': delegation_date} #Unico requerido
+                'delegation_date': delegation_date}  # Unico requerido
         form = TaskForm(data=data)
         print(form.errors)
-        self.assertTrue(form.is_valid())    
+        self.assertTrue(form.is_valid())
 
     def test_list_view(self):
-    
         url = reverse('task_list')
         resp = self.client.get(url)
 
         self.assertEqual(resp.status_code, 200)
-    
+
     def test_create_view(self):
         movement = mommy.make(Movement).id
         url = reverse('task_add', kwargs={'movement': movement})
         resp = self.client.get(url)
 
         self.assertEqual(resp.status_code, 200)
-    
+
     def test_update_view(self):
-        #Task tem algumas fk, o que pode causar erros se instanciados diretamente pelo mommy.
-        #Neste caso deve-se instanciar cada objeto separadamente
-        mommy.make(ContactMechanismType, name='email')
+        # Task tem algumas fk, o que pode causar erros se instanciados diretamente pelo mommy.
+        # Neste caso deve-se instanciar cada objeto separadamente
         c_inst = mommy.make(Task, movement=mommy.make(Movement, legacy_code='999'),
                             person_asked_by=mommy.make(Person, is_active=True, is_lawyer=True),
                             person_executed_by=mommy.make(Person, is_lawyer=True),
                             type_task=mommy.make(TypeTask))
-        
-        print("Printou",c_inst.id)
+
+        print("Printou", c_inst.id)
         url = reverse('task_update', kwargs={'pk': str(c_inst.id), 'movement': c_inst.movement.id})
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
 
     def test_delete_view(self):
-        mommy.make(ContactMechanismType, name='email')
         c_inst = mommy.make(Task)
-        data = {'task_list': {c_inst.id}}
+        data = {'task_list': {c_inst.id}, 'movement': c_inst.movement.id}
         url = reverse('task_delete')
         resp = self.client.post(url, data, follow=True)
         self.assertEqual(resp.status_code, 200)
 
 
 class TaskHistoryTest(TestCase):
-
     def test_model(self):
-        
-        #mommy deixa as coisas bem mais faaceis
-        mommy.make(ContactMechanismType, name='email')
+        # mommy deixa as coisas bem mais faaceis
         c_inst = mommy.make(TaskHistory)
         self.assertTrue(isinstance(c_inst, TaskHistory))
-        
-class EcmTest(TestCase):
 
+
+class EcmTest(TestCase):
     def setUp(self):
-        
         user = User.objects.create_user(username='username', password='password')
         self.client.login(username='username', password='password')
-
 
     def test_model(self):
         # mommy deixa as coisas bem mais faaceis
         c_inst = mommy.make(Ecm)
-        self.assertTrue(isinstance(c_inst, Ecm))  # Para fazer esse test e preciso criar uma pasta em /opt/files_easy_lawyer com permissao para escrita
-        
+        self.assertTrue(isinstance(c_inst,
+                                   Ecm))  # Para fazer esse test e preciso criar uma pasta em /opt/files_easy_lawyer com permissao para escrita
+
     def test_valid_EcmForm(self):
         task = mommy.make(Task).id
-        file_path = 'arquivo_exemplo.txt' #tem que ta na pasta raiz
+        file_path = 'arquivo_exemplo.txt'  # tem que ta na pasta raiz
         path = SimpleUploadedFile(name=file_path, content=open(file_path, 'rb').read())
-        data = {'task':task}
-        file_dict = {'path':path}
-        form = EcmForm(data=data,files=file_dict) #ATENCAO!!!!!!!!! ARQUIVO SE PASSA PELO PARAMETRO "files" (oh god D:)
+        data = {'task': task}
+        file_dict = {'path': path}
+        form = EcmForm(data=data,
+                       files=file_dict)  # ATENCAO!!!!!!!!! ARQUIVO SE PASSA PELO PARAMETRO "files" (oh god D:)
         print(form.errors)
         self.assertTrue(form.is_valid())
 
@@ -178,16 +159,22 @@ class EcmTest(TestCase):
         resp = self.client.post(url)
 
         self.assertEqual(resp.status_code, 200)
-    
+
     def test_delete_view(self):
-        task = mommy.make(Task,movement=mommy.make(Movement,legacy_code='999'),
-                                 person_asked_by=mommy.make(Person,name='joao',is_active=True,is_lawyer=True),
-                                 person_executed_by=mommy.make(Person,name='pedro',is_lawyer=True),
-                                 type_task=mommy.make(TypeTask,name='TT123')).id       
-        url = reverse('delete_ecm',kwargs={'pk':task})
+        task = mommy.make(Task, movement=mommy.make(Movement, legacy_code='999'),
+                          person_asked_by=mommy.make(Person, name='joao', is_active=True, is_lawyer=True),
+                          person_executed_by=mommy.make(Person, name='pedro', is_lawyer=True),
+                          type_task=mommy.make(TypeTask, name='TT123')).id
+        url = reverse('delete_ecm', kwargs={'pk': task})
         resp = self.client.post(url)
 
-        self.assertEqual(resp.status_code, 200)    
-        
-        
-        
+        self.assertEqual(resp.status_code, 200)
+
+
+class TaskDetailTest(TestCase):
+    def test_valid_TaskDetailForm(self):
+        data = {'execution_date': datetime.date.today(),
+                'survey_result': 'cumprido', 'notes': 'teste'}
+        form = TaskDetailForm(data=data)
+        print(form.errors)
+        self.assertTrue(form.is_valid())
