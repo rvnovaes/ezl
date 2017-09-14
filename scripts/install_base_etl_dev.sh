ec5646b335fd7b2aabd912b2bc67d2f0daa716ac
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2017-09-14 11:07:27 -0300

    0000458: Acertar erros do ETL

--- scripts/install_base_etl_dev.sh
+++ scripts/install_base_etl_dev.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
+# O que faz este script?
+#    - Clona o código
+#    - Instala as dependencias utilizando um ambiente no virtualenv
+#    - Cria um banco de teste
+#    - Instala o a extensão unnacent no banco
+#    - Roda as migrations
+#    - Chama a execução da ETL com o sistema ADVWin_HO
+#
+# O que preciso para rodar este script?
+#    - É nescessário ter instalado em uma máquina com Ubuntu ou derivados do Debian
+#      - PostgresSQL
+#      - Um usuário no banco com o nome ezl
+#      - Python3.5
+#    - Ter o usuário cadastrado no repositório
+#
+# Como rodar este script²
+#    - Basta entrar no diretório onde ele se encontra
+#    - Digitar o comando ./install_base_etl_dev.sh
+#    - É necessário dar permissão de execução no script caso ele não tenha
+#      - O comando é: sudo chmod +x install_base_etl_dev.sh
+#
+# Porque rodar este script?
+#    - A realização bem-sucedida deste script permite a automatização deste processo e garantia que o básico
+#    - para acesso ao sistema e importacao dos dados do ADVWin esta funcionando.
+#
+# Quando rodar este script?
+#    - Em ambientes de desenvolvimento (Através deste será criado novos scripts para ambientes de demo e produção)
+#
+# Observações:
+#    - Este script funciona na rede interna da empresa
+#    - Para rodar em redes externas, é necessário configurar o tunelamento
+#      da conexao com o servidor do advwin e alterar neste script
+#   -  na sessão 'Configurando ETL' o valor server = 172.27.155.9 para server = 127.0.0.1:2000
+
+
+
+
 # Diretorio padrao para execucao do projeto do zero
 base_dir='/opt/easy_lawyer_teste'
 dir=$base_dir/teste_`date +'%Y%m%d%H%M%S'`
@@ -65,7 +102,7 @@ sudo kill -9 `ps -aux | grep -i luigi_jobs.py | awk {'print $2'} | head -1`
 # Configurando a ETL
 sudo echo "
 [connection]
-server = 127.0.0.1:2000
+server = 172.27.155.9
 user = Rvnovaes
 password = libertas
 database = Advwin_ho
