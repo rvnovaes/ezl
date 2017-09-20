73c7785beb55cb5eea006d01e0c9a1219253904f
Luiz Fernando Moreira Rodrigues <dggccgn@gmail.com>
2017-09-20 10:44:31 -0300

    [432]: Alteracoes no script restore_database e no template

--- restore_database.py
+++ restore_database.py
@@ -23,17 +23,12 @@ password = sys.argv[2]
 if username == current_user_db and password == current_password_db:
     path_dump_db = '/opt/demo.dump'
 
-    print('<br><br>')
-    print('A aplicação EZL Demo irá ficar indisponível durante este processo')
-    print('<br><br>')
     # Para o serviço (demo) do Django e reinicia o serviço do Postgres para limpar as sessões ativas
     subprocess.call('sudo supervisorctl stop demo_uwsgi', shell=True)
     subprocess.call('sudo /etc/init.d/postgresql restart', shell=True)
 
     # Limpa os dados de todas as tabelas do EZL
     db_name = settings.DATABASES['default']['NAME']
-    print('Limpando o banco ' + current_db_name + '....')
-    print('<br><br>')
     subprocess.call('sudo python3 manage.py flush --noinput', shell=True)
 
     # O comando python3 manage.py flush não apaga os dados das tabelas auth_permission, django_content_type, django_migrations, django_site.
@@ -44,24 +39,28 @@ if username == current_user_db and password == current_password_db:
     cursor.execute("TRUNCATE TABLE django_content_type CASCADE")
     cursor.execute("TRUNCATE TABLE django_migrations CASCADE")
     cursor.execute("TRUNCATE TABLE django_site CASCADE")
-
-    print('Banco ' + db_name + ' limpado com sucesso')
-    print('<br><br>')
+    cursor.execute("SELECT * FROM task")
+    quant_after_clear = len(cursor.fetchall())
 
     # Realiza o restore do banco
 
-    print('Realizando pg_restore no banco ' + current_db_name + ' ...')
-    print('<br><br>')
     pg_restore = 'pg_restore -a --disable-triggers -U ezl -d ' + current_db_name + ' ' + path_dump_db
     # pg_restore = 'psql -U ezl demo < /opt/ezl.dump'
 
     subprocess.call(pg_restore, shell=True)
-    print('Processo  concluído !')
-    print('<br><br>')
+    cursor.execute("SELECT * FROM task")
+    quant_after_restore = len(cursor.fetchall())
 
     # Inicia o serviço (demo) do Django para disponibilizar a aplicacao novamente ao  usuário
     subprocess.call('sudo supervisorctl start demo_uwsgi', shell=True)
-    print('A aplicação EZL Demo já disponível para acesso !')
+
+    if quant_after_restore > quant_after_clear:
+        print('. Recuperação realizada com sucesso !')
+
+    else:
+        print('. Ocorreu um erro ao recuperar o banco !')
+
+
 
 else:
     print('Usuário e/ou senha do banco de dados não estão correto(s).')
