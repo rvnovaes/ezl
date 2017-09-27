bd06f263234c23f5893b1a36c5b3110806ae5321
Luiz Fernando Moreira Rodrigues <dggccgn@gmail.com>
2017-09-27 15:02:14 -0300

    [478]: Acertar erros do ETL de type movement

--- etl/advwin_ezl/law_suit/type_movement.py
+++ etl/advwin_ezl/law_suit/type_movement.py
@@ -1,24 +1,32 @@
 from core.utils import LegacySystem
-from etl.advwin_ezl.advwin_ezl import GenericETL
+from etl.advwin_ezl.advwin_ezl import GenericETL, validate_import
 from lawsuit.models import TypeMovement
 
 
 class TypeMovementETL(GenericETL):
-    import_query = "SELECT  tm.Codigo,  tm.Descricao FROM Jurid_CodMov AS tm  " \
-                   "WHERE right(tm.Codigo, 1) <> '.'  " \
-            "   AND (tm.UsarOS = 0 OR tm.UsarOS IS NULL)  " \
-            "   AND tm.Status = 'Ativo'  " \
-            "   AND tm.Codigo= (SELECT MIN(tm2.codigo)    " \
-            "                   FROM Jurid_CodMov AS tm2 WHERE tm.Descricao = tm2.Descricao)"
+    import_query = """
+                    SELECT  tm.Codigo AS legacy_code,
+                            tm.Descricao 
+                      
+                      FROM Jurid_CodMov AS tm 
+                      
+                      WHERE right(tm.Codigo, 1) <> '.'  
+                            AND (tm.UsarOS = 0 OR tm.UsarOS IS NULL)  
+                            AND tm.Status = 'Ativo'  
+                            AND tm.Codigo= (SELECT MIN(tm2.codigo)   
+                      FROM Jurid_CodMov AS tm2 WHERE tm.Descricao = tm2.Descricao)
+                                               
+                   """
     model = TypeMovement
     advwin_table = 'Jurid_CodMov'
     has_status = True
 
+    @validate_import
     def config_import(self, rows, user, rows_count):
         for row in rows:
 
             try:
-                code = row['Codigo']
+                code = row['legacy_code']
                 name = row['Descricao']
 
                 # tem que verificar se é novo antes para não salvar o create_user ao fazer update
@@ -37,14 +45,14 @@ class TypeMovementETL(GenericETL):
                                               create_user=user,
                                               alter_user=user)
                 self.debug_logger.debug(
-                    "Tipo Movimentacao,%s,%s,%s,%s,%s,%s,%s" % (str(name), str(True), str(code), str(LegacySystem.ADVWIN.value),
-                                                     str(user.id),str(user.id),self.timestr))
+                    "Tipo Movimentacao,%s,%s,%s,%s,%s,%s,%s" % (
+                    str(name), str(True), str(code), str(LegacySystem.ADVWIN.value),
+                    str(user.id), str(user.id), self.timestr))
 
             except Exception as e:
                 self.error_logger.error(
                     "Ocorreu o seguinte erro na importacao de Tipo Movimentacao: " + str(rows_count) + "," + str(
                         e) + "," + self.timestr)
-
 
         super(TypeMovementETL, self).config_import(rows, user, rows_count)
 
