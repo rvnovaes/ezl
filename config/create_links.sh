0dedd24daca8bf5a9672f00d9742cc29d4f13964
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2017-09-28 16:58:45 -0300

    movidos os arquivos de configuracao do supervisor e do uwsgi para a pasta config e criados os links

--- config/create_links.sh
+++ config/create_links.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-# Datadog
+# datadog
 ln -s /opt/easy_lawyer_django/config/datadog/nginx.yaml /etc/dd-agent/conf.d
 
-
-# Nginx
+# nginx
 ln -s /opt/easy_lawyer_django/config/nginx/nginx.conf /etc/nginx/sites-enabled
+
+# supervisor
+ln -s /opt/easy_lawyer_django/config/supervisor/supervisord.conf /etc/supervisor
+ln -s /opt/easy_lawyer_django/config/supervisor/ezl.conf /etc/supervisor/conf.d
