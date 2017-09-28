b978a12829e342972d0c3432aa8c6d50f269e903
Roberto Vasconcelos Novaes <rvnovaes@gmail.com>
2017-09-28 15:19:05 -0300

    movido arquivo do ngnix do ezl para config, incluida configuracao da url do agente do datadog e criado link

--- config/create_links.sh
+++ config/create_links.sh
@@ -2,3 +2,7 @@
 
 # Datadog
 ln -s /opt/easy_lawyer_django/config/datadog/nginx.yaml /etc/dd-agent/conf.d
+
+
+# Nginx
+ln -s /opt/easy_lawyer_django/config/nginx/nginx.conf /etc/nginx/sites-enabled
