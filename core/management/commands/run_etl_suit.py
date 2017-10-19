e5206479f1b92536c455948b83f8434e56121bd1
Paulo <proberto.macedo@gmail.com>
2017-10-19 14:45:50 -0200

    Melhorado comando run_etl_suit

--- core/management/commands/run_etl_suit.py
+++ core/management/commands/run_etl_suit.py
@@ -4,6 +4,10 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Execute ETL suit'
 
+    def run_person_etl(self):
+        from etl.advwin_ezl.core.person import PersonETL
+        PersonETL().import_data()
+
     def run_user_etl(self):
         from etl.advwin_ezl.account.user import UserETL
         UserETL().import_data()
@@ -14,5 +18,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.run_ecm_etl()
-
-
+        self.run_user_etl()
+        self.run_person_etl()
