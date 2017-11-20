51cd0a36964be0ee53a73115d9b9877fdbf96dee
Ervilis Viana de Souza <ervilisviana@gmail.com>
2017-11-20 15:15:48 -0200

    Voltando filtro do sql removido

--- etl/advwin_ezl/law_suit/folder.py
+++ etl/advwin_ezl/law_suit/folder.py
@@ -27,7 +27,7 @@ class FolderETL(GenericETL):
               p.Cliente IS NOT NULL AND p.Cliente <> '' AND
               ((a.prazo_lido = 0 AND a.SubStatus = 30) OR
               (a.SubStatus = 80)) AND a.Status = '0' -- STATUS ATIVO
-
+              AND a.Advogado='12157458697' -- marcio.batista (Em teste)
                   """
     has_status = True
 
