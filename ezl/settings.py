21d72dfbcd8f7febb0da88a79dfa48099322894d
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2017-12-04 16:35:57 -0200

    incluir e-mail v?lido para envio de recupera??o de senha

--- ezl/settings.py
+++ ezl/settings.py
@@ -234,6 +234,7 @@ EMAIL_HOST = email_host
 EMAIL_PORT = email_port
 EMAIL_HOST_USER = email_host_user
 EMAIL_HOST_PASSWORD = email_host_password
+DEFAULT_FROM_EMAIL = email_host_user
 
 
 INTERNAL_IPS = '127.0.0.1'
