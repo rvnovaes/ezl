28aaae36b4a0588553663d9e33fda6ddbb9d72c1
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2017-12-20 09:44:17 -0200

    Inserido Status 'Especial' nas ETL de Folder, Law_Suit, Movement, Task e Ecm_Task

--- etl/advwin_ezl/law_suit/folder.py
+++ etl/advwin_ezl/law_suit/folder.py
@@ -25,7 +25,7 @@ class FolderETL(GenericETL):
                     a.CodMov = cm.Codigo
             WHERE
               cm.UsarOS = 1 AND
-              p.Status = 'Ativa' AND
+              (p.Status = 'Ativa' OR p.Status = 'Especial') AND
               p.Codigo_Comp IS NOT NULL AND p.Codigo_Comp <> '' AND
               p.Cliente IS NOT NULL AND p.Cliente <> '' AND
               ((a.prazo_lido = 0 AND a.SubStatus = 30) OR
