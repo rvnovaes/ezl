eae6aa6eeb679e20f5186a9ccc89e8fa81fc2feb
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2017-12-26 18:57:25 -0200

    Implementando contador de mensagens n?o lidas

--- chat/static/chat/js/chat.js
+++ chat/static/chat/js/chat.js
@@ -1,12 +1,31 @@
+var setBadgeItem = function (items) {
+    items.forEach(function (item) {
+        var el = $("[badge-id=" + item.message__chat__pk + "]")
+        if (item.quantity){
+            el.text(item.quantity)
+        } else{
+            el.text(null)
+        }
+
+    })
+};
+
 setInterval(function () {
     $.ajax({
         type: "GET",
         url: "/chat/count_message/",
         data: {},
         success: function(response){
-            $('.chat-badge.badge').text(response.count)
+            console.log(response.all_messages);
+            if (response.all_messages > 0) {
+                $('.chat-badge.badge').text(response.all_messages)
+            } else {
+                $('.chat-badge.badge').text(null)
+            }
+            setBadgeItem(response.grouped_messages)
         },
         dataType: "json"
     });
-    console.log($('.chat.badge').text())
 }, 1000);
+
+
--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,8 @@ from chat.models import Chat, UnreadMessage
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.views.generic import View
+from django.db.models import Count
+import json
 
 
 class ChatListView(ListView):
@@ -20,7 +22,12 @@ class ChatListView(ListView):
 
 class ChatCountMessages(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        return JsonResponse({
-            'count': UnreadMessage.objects.filter(
-                user_by_message__user_by_chat=self.request.user).count()
-        })
+        return JsonResponse(
+            {'all_messages': UnreadMessage.objects.filter(
+                user_by_message__user_by_chat=self.request.user).count(),
+             'grouped_messages': list(UnreadMessage.objects.filter(
+                 user_by_message__user_by_chat=self.request.user).values(
+                 'message__chat__pk').annotate(
+                 quantity=Count('id')).order_by())
+             }
+        )
