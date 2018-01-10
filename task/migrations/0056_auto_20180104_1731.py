04629bce5434d46eda3941efb6e365bd52b31e27
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2018-01-10 09:45:05 -0200

    Layout campos de data

--- task/migrations/0056_auto_20180104_1731.py
+++ task/migrations/0056_auto_20180104_1731.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('task', '0055_auto_20171227_1354'),
+        ('task', '0055_auto_20180104_1731'),
     ]
 
     operations = [
