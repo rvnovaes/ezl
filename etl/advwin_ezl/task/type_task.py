891ac87b633d963b782399d7c819b1d7356d897b
Iasmini Gomes <5900939+iasmini@users.noreply.github.com>
2018-01-19 15:19:59 -0200

    EZL-73: trocado o surve_id pelo objeto survey

--- etl/advwin_ezl/task/type_task.py
+++ etl/advwin_ezl/task/type_task.py
@@ -1,7 +1,7 @@
 from core.utils import LegacySystem
 from etl.advwin_ezl.advwin_ezl import GenericETL, validate_import
 from task.models import TypeTask
-from survey.models import LegacySurveyType
+from survey.models import LegacySurveyType, Survey
 from etl.utils import get_message_log_default, save_error_log
 
 
@@ -41,31 +41,34 @@ class TypeTaskETL(GenericETL):
                 survey_key = row['formulario_id']
                 survey_id = survey_map[survey_key]
 
+                survey = Survey.objects.filter(id=survey_id)
+
                 # tem que verificar se é novo antes para não salvar o create_user ao fazer update
                 instance = self.model.objects.filter(legacy_code=code,
                                                      system_prefix=LegacySystem.ADVWIN.value).first()
                 if instance:
                     instance.name = name
-                      instance.survey = survey_id
+                    instance.survey = survey
                     instance.alter_user = user
                     instance.is_active = True
-                    instance.save(update_fields=['is_active', 'name', 'alter_user', 'alter_date', 'survey'])
+                    instance.legacy_code = code
+                    instance.save(update_fields=['is_active', 'name', 'alter_user', 'alter_date', 'survey',
+                                                 'legacy_code'])
                 else:
                     self.model.objects.create(name=name,
                                               is_active=True,
                                               legacy_code=code,
-                                              survey=survey_id,
+                                              survey=survey,
                                               system_prefix=LegacySystem.ADVWIN.value,
                                               create_user=user,
                                               alter_user=user)
 
                 self.debug_logger.debug(
                     "Type Task,%s,%s,%s,%s,%s,%s,%s,%s" % (str(name), str(True), str(code), str(survey_id),
-                                                         str(LegacySystem.ADVWIN.value), str(user.id),str(user.id),
-                                                         self.timestr))
+                                                           str(LegacySystem.ADVWIN.value), str(user.id),str(user.id),
+                                                           self.timestr))
             except Exception as e:
-                msg = get_message_log_default(self.model._meta.verbose_name,
-                                              rows_count, e, self.timestr)
+                msg = get_message_log_default(self.model._meta.verbose_name, rows_count, e, self.timestr)
                 self.error_logger.error(msg)
                 save_error_log(log, user, msg)
 
