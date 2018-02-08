0635176c958fb2586b2be5896c817bdd8be9207f
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2018-02-08 16:29:41 -0200

    Migrations Unique_togheter com office

--- task/models.py
+++ task/models.py
@@ -166,6 +166,8 @@ class Task(Audit, LegacyCode, OfficeMixin):
     execution_date = models.DateTimeField(null=True, verbose_name='Data de Cumprimento')
 
     requested_date = models.DateTimeField(null=True, verbose_name='Data de Solicitação')
+    acceptance_service_date = models.DateTimeField(null=True, verbose_name='Data de Aceitação pelo Contratante')
+    refused_service_date = models.DateTimeField(null=True, verbose_name='Data de Recusa pelo Contratante')
     return_date = models.DateTimeField(null=True, verbose_name='Data de Retorno')
     refused_date = models.DateTimeField(null=True, verbose_name='Data de Recusa')
 
