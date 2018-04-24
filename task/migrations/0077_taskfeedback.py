a1b95ec65b3fe650d38edb675d9931fbbcab8ccd
Iasmini Gomes <5900939+iasmini@users.noreply.github.com>
2018-04-24 13:25:20 -0300

    acerto em merge de migration com mesma numeracao

--- task/migrations/0077_taskfeedback.py
+++ task/migrations/0077_taskfeedback.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('task', '0075_taskgeolocation'),
+        ('task', '0076_auto_20180413_1408'),
     ]
 
     operations = [
