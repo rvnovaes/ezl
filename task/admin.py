4d927ef5e184ba240a27cf087fcbf88f9bf72924
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2018-05-08 11:00:07 -0300

    Adicionado tipo de servico no django admin

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import TypeTask
 # Register your models here.
+
+
+@admin.register(TypeTask)
+class TypeTaskModelAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'survey']
+    list_display_links = ['pk', 'name']
+    search_fields = ['name']
+    fields = ['create_user', 'name', 'survey']
+    ordering = ['name']
