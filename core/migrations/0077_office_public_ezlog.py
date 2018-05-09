354d692070632fa9d16bace6d488fc78d38acd81
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2018-05-09 19:13:15 -0300

    Criando compo public_office e migration para criar o office EZLog automaticamente

---
+++ core/migrations/0077_office_public_ezlog.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+def create_office_ezlog(apps, schema_editor):
+	from core.models import Office
+	from django.contrib.auth.models import User
+
+	user = User.objects.get(username='admin')	
+	office = Office()
+	office.name = 'EZLog'
+	office.legal_name = 'EZLog'
+	office.public_office = True
+	office.create_user = user
+	office.save()
+	office.officemembership_set.create(person=user.person, create_user=user)
+
+
+class Migration(migrations.Migration):
+	dependencies = [
+		('core', '0076_office_public_office'), 
+	]
+
+	operations = [
+		migrations.RunPython(create_office_ezlog), 
+	]
