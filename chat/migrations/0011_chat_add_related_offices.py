b7d46b0d27ee8a22f2fa2f3f4266f9e92de35684
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2018-05-09 11:27:33 -0300

    Ajustando migration para ordernar chat pelas ultimas mensagens

--- chat/migrations/0011_chat_add_related_offices.py
+++ chat/migrations/0011_chat_add_related_offices.py
@@ -7,8 +7,17 @@ from django.db import migrations
 
 def chat_add_user_by_office(apps, schema_editor):
     from task.models import Task
+    from chat.models import Message
     from task.signals import create_or_update_chat
-    for task in Task.objects.all():
+    from django.db.models import Q
+    last_tasks_to_update = list(set(map(lambda i: Task.objects.filter(chat=i.chat).first().pk, Message.objects.all().order_by('-create_date'))))
+
+    for task in Task.objects.filter(~Q(chat__pk__in=last_tasks_to_update)):
+        create_or_update_chat(Task, task, False)
+
+    # Nessario para manter o chat ordenado pelas ultimas mensagens
+    for task_id in last_tasks_to_update:
+        task = Task.objects.get(pk=task_id)
         create_or_update_chat(Task, task, False)
 
 class Migration(migrations.Migration):
