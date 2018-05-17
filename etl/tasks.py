1460453c9bbfc523ca2bd394d3b708033dabcb69
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2018-05-17 12:04:18 -0300

    tarefa no celery para apagar registros anteriores a 15 dias, as 2h da manha

---
+++ etl/tasks.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, unicode_literals
+from celery import task
+from django.utils import timezone
+from datetime import timedelta
+from etl.models import DashboardETL
+
+
+@task()
+def remove_old_etldashboard():
+    date_limit = timezone.now() - timedelta(days=15)
+    dashboards = DashboardETL.objects.filter(create_date__lte=date_limit)
+    ret = {'total': DashboardETL.objects.all().count(), 'excluidos': dashboards.count()}
+    dashboards.delete()
+    return ret
