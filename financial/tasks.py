0d9b84df4196b4bccab0d440a2c199d43df7bd7e
Flávio Andrei <andrei.flavio@hotmail.com>
2018-05-25 14:49:22 -0300

    Ajustes no cancelamento do processo;  In?cio de implementa??o do log do celery para poder exibir seu resultado para o usu?rio ao final da importa??o.

--- financial/tasks.py
+++ financial/tasks.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.contrib.auth.models import User
 from django.core.cache import cache
 from celery import shared_task, current_task
+from celery.utils.log import get_task_logger
 from openpyxl import load_workbook
 from djmoney.money import Money
 from .models import ServicePriceTable, ImportServicePriceTable
@@ -12,12 +13,12 @@ from lawsuit.models import CourtDistrict
 
 IMPORTED_IMPORT_SERVICE_TABLE = 'imported_ispt_'
 PROCESS_PERCENT_IMPORT_SERVICE_PRICE_TABLE = 'process_percent_ispt_'
- 
+
+# lista para montar log de erros da importação
+logger = get_task_logger('ezl.financial')
+
 @shared_task
-def import_xls_service_price_table(file_id):    
-    # lista para montar log de erros da importação    
-    errors = []
-    
+def import_xls_service_price_table(file_id):        
     xls_file = ImportServicePriceTable.objects.get(pk = file_id)
     cache.set(IMPORTED_IMPORT_SERVICE_TABLE + str(xls_file.pk), False, timeout=None)
     
@@ -43,7 +44,7 @@ def import_xls_service_price_table(file_id):
                 office_correspondent = Office.objects.filter(legal_name__unaccent__iexact=office_name).first()
 
                 if office_correspondent is None:
-                    errors.append('Escritório correspondente %s não encontrado' % office_name)                    
+                    logger.info('Escritório correspondente %s não encontrado' % office_name)                    
                     importar = False                    
                 
             # serviço            
@@ -55,7 +56,7 @@ def import_xls_service_price_table(file_id):
                 type_task = TypeTask.objects.filter(name__unaccent__iexact=nome_servico).first()
 
                 if type_task is None:
-                    errors.append('Tipo de serviço %s não encontrado' % nome_servico)
+                    logger.info('Tipo de serviço %s não encontrado' % nome_servico)
                     importar = False                    
 
             # uf            
@@ -64,7 +65,7 @@ def import_xls_service_price_table(file_id):
             if sigla_uf != '':
                 state = State.objects.filter(initials__iexact=sigla_uf).first()
                 if state is None:
-                    errors.append('UF %s não encontrada' % sigla_uf)                    
+                    logger.info('UF %s não encontrada' % sigla_uf)                    
                     importar = False
             
             # Comarca
@@ -96,7 +97,7 @@ def import_xls_service_price_table(file_id):
                         value = row[4].value
                 except:
                     value = 0
-                    errors.append('Valor {} inválido. Verificar escritório {}, comarca {}, estado {}.'.format(
+                    logger.info('Valor {} inválido. Verificar escritório {}, comarca {}, estado {}.'.format(
                         row[4].value, office_correspondent, court_district, state))                                
                 
                 if service_price_table is None:                    
@@ -130,9 +131,7 @@ def import_xls_service_price_table(file_id):
         i = i + 1
         cache.set(PROCESS_PERCENT_IMPORT_SERVICE_PRICE_TABLE + str(xls_file.pk), process_percent, timeout=None)            
     
-    cache.set(IMPORTED_IMPORT_SERVICE_TABLE + str(xls_file.pk), True, timeout=None)            
-    
-    return errors
+    cache.set(IMPORTED_IMPORT_SERVICE_TABLE + str(xls_file.pk), True, timeout=None)
 
 
 def remove_caracter_especial(texto):
