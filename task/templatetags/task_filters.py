3411534804859cf1955ef8842bf81aab4ac509cc
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2018-06-26 15:46:08 -0300

    botao de recusar duplicanco

--- task/templatetags/task_filters.py
+++ task/templatetags/task_filters.py
@@ -12,3 +12,17 @@ def valid_status(status):
         return TaskStatus(status) in [TaskStatus.REQUESTED, TaskStatus.ACCEPTED_SERVICE, TaskStatus.OPEN,
                                       TaskStatus.ACCEPTED]
     return False
+
+
+@register.filter
+def get_refused_action(user, task):
+    refused_action = None
+    if task.status.name == 'OPEN' or task.status.name == 'ACCEPTED':
+        if task.person_executed_by == user.person:
+            if task.status.name == 'OPEN':
+                refused_action = 'REFUSED'
+        elif not task.get_child:
+            refused_action = 'REQUESTED'
+        elif task.get_child:
+            refused_action = 'REQUESTED' if valid_status(task.get_child.task_status) else 'INVALID_CHILD_STATUS'
+    return refused_action
