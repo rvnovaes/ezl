e2e93e3121fe565decd394c238f95e2f6fce742f
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2018-07-09 15:35:45 -0300

    mudanca de migrations para permitir executar com o banco zerado

--- core/migrations/0081_office_public_ezlog.py
+++ core/migrations/0081_office_public_ezlog.py
@@ -3,25 +3,28 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-def create_office_ezlog(apps, schema_editor):
-	from core.models import Office
-	from django.contrib.auth.models import User
 
-	user = User.objects.get(username='admin')	
-	office = Office()
-	office.name = 'EZLog'
-	office.legal_name = 'EZLog'
-	office.public_office = True
-	office.create_user = user
-	office.save()
-	office.officemembership_set.create(person=user.person, create_user=user)
+def create_office_ezlog(apps, schema_editor):
+    from core.models import Office
+    from django.contrib.auth.models import User
+
+    user = User.objects.filter(username='admin').first()
+    if not user:
+        return True
+    office = Office()
+    office.name = 'EZLog'
+    office.legal_name = 'EZLog'
+    office.public_office = True
+    office.create_user = user
+    office.save()
+    office.officemembership_set.create(person=user.person, create_user=user)
 
 
 class Migration(migrations.Migration):
-	dependencies = [
-		('core', '0080_contactmechanismtype_type_contact_mechanism'), 
-	]
+    dependencies = [
+        ('core', '0080_contactmechanismtype_type_contact_mechanism'),
+    ]
 
-	operations = [
-		migrations.RunPython(create_office_ezlog), 
-	]
+    operations = [
+        migrations.RunPython(create_office_ezlog),
+    ]
