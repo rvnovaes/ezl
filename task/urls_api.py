7d6b335876e161220fcad69df52177f5eccaa7f6
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2018-07-10 20:27:49 -0300

    Criando endpoint de task

--- task/urls_api.py
+++ task/urls_api.py
@@ -5,5 +5,6 @@ from . import views_api as views
 
 router = routers.SimpleRouter()
 router.register(r'type_task', views.TypeTaskViewSet, base_name='type_task')
+router.register(r'task', views.TaskViewSet, base_name='task')
 
 urlpatterns = []
