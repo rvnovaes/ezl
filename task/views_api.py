ba635a77046d3af184d60ec1e98b3b434201b83b
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2018-07-12 11:30:05 -0300

    Implementando filtros

--- task/views_api.py
+++ task/views_api.py
@@ -1,7 +1,9 @@
 from .models import TypeTask, Task
 from .serializers import TypeTaskSerializer, TaskSerializer
+from .filters import TaskApiFilter
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class TypeTaskViewSet(viewsets.ReadOnlyModelViewSet):
@@ -14,3 +16,5 @@ class TypeTaskViewSet(viewsets.ReadOnlyModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
 	queryset = Task.objects.all()
 	serializer_class = TaskSerializer
+	filter_backends = (DjangoFilterBackend,)
+	filter_class = TaskApiFilter
