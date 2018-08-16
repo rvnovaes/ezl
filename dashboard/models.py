4d03c82480d44ac10976dee6f31491de1e490066
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2018-08-16 12:20:34 -0300

    Ajustando migrations

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,6 +7,8 @@ from .schemas import *
 
 
 
+
+
 class Dashboard(models.Model):
 	company = models.ForeignKey(Company, verbose_name='Empresa')
 	refresh = models.IntegerField(verbose_name='Refresh por millesegundo', blank=True, null=True)	
@@ -18,10 +20,12 @@ class Dashboard(models.Model):
 class Card(models.Model):		
 	name = models.CharField(verbose_name='Nome', max_length=255, blank=True, null=True)
 	code = models.TextField(verbose_name='Codigo', blank=True, null=True)	
-	dashboards = models.ManyToManyField(Dashboard, related_name='cards', null=True, blank=True)
+	dashboards = models.ManyToManyField(Dashboard, related_name='cards', through='DashboardCard', blank=True)
 	schema = JSONField(verbose_name=u'Schema', blank=True, null=True, default=json.dumps(CARD))
 
 	def __str__(self):
 		return self.name
 
-
+class DashboardCard(models.Model):
+    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE, blank=True)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, blank=True)
