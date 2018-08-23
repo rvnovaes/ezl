fe0ff951ead59de82205a304c0667e625012e29b
Iasmini Gomes <5900939+iasmini@users.noreply.github.com>
2018-08-23 18:40:18 -0300

    acerto na mesclagem das migrations

--- ecm/migrations/0007_attachment_size.py
+++ ecm/migrations/0007_attachment_size.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ecm', '0005_auto_20180509_0946'),
+        ('ecm', '0006_defaultattachmentrule'),
     ]
 
     operations = [
