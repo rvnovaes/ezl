3ac780481ba7623f2b3cb5a016e2b2f69eef71ba
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2018-08-24 10:40:31 -0300

    filtro do bulk create de OS por office da sessao

--- lawsuit/views_api.py
+++ lawsuit/views_api.py
@@ -3,7 +3,13 @@ from django.http import JsonResponse
 from django.views import View
 from .models import LawSuit, Movement
 from core.api import ApiViewMixin
+from core.utils import get_office_session
 from task.models import Task
+
+
+def office_filter(queryset, request):
+    office = get_office_session(request)
+    return queryset.filter(office=office)
 
 
 class LawsuitApiView(LoginRequiredMixin, ApiViewMixin):
@@ -15,7 +21,7 @@ class LawsuitApiView(LoginRequiredMixin, ApiViewMixin):
             raise self.bad_request("folder is required")
 
         queryset = queryset.filter(folder_id=folder)
-        return queryset
+        return office_filter(queryset, self.request)
 
 
 class MovementApiView(LoginRequiredMixin, ApiViewMixin):
@@ -27,7 +33,7 @@ class MovementApiView(LoginRequiredMixin, ApiViewMixin):
             raise self.bad_request("lawsuit is required")
 
         queryset = queryset.filter(law_suit_id=lawsuit)
-        return queryset
+        return office_filter(queryset, self.request)
 
 
 class TaskApiView(LoginRequiredMixin, ApiViewMixin):
@@ -39,4 +45,4 @@ class TaskApiView(LoginRequiredMixin, ApiViewMixin):
             raise self.bad_request("movement is required")
 
         queryset = queryset.filter(movement_id=movement)
-        return queryset
+        return office_filter(queryset, self.request)
