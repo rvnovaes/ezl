a42b31026b33d348d4e81255299116beb1b2d146
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2018-09-26 14:22:02 -0300

    exibicao de botao faturar e receber nos relatorios

--- task/views.py
+++ task/views.py
@@ -251,7 +251,7 @@ class TaskReportBase(PermissionRequiredMixin, CustomLoginRequiredView, TemplateV
                 office_list, total = self.get_os_grouped_by_client()
         except:
             office_list, total = self.get_os_grouped_by_office()
-        context['offices'] = office_list
+        context['offices_report'] = office_list
         context['total'] = total
         return context
 
