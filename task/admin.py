7a5712251236f0e77031b8cd906555819256851d
Ervilis Viana de Souza <ervilisviana@gmail.com>
2018-10-04 18:45:49 -0300

    Adicionando EcmTask ao Django Admin

--- task/admin.py
+++ task/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TypeTask, TypeTaskMain
+from .models import TypeTask, TypeTaskMain, EcmTask
 from .forms import TypeTaskMainForm
 
 
@@ -23,3 +23,15 @@ class TypeTaskMainAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'is_hearing']
     list_display_links = ['pk', 'name']
     search_fields = ['name', 'characteristics']
+
+
+@admin.register(EcmTask)
+class EcmTask(admin.ModelAdmin):
+    list_display = ['task_id', 'ecm_id']
+    search_fields = ['task_id', 'ecm_id']
+
+    def task_id(self, instance):
+        return instance.task_id
+
+    def ecm_id(self, instance):
+        return instance.ecm_id
