53db2377fa94c758f2e4bff6677c9cfa763dca58
Iasmini Gomes <5900939+iasmini@users.noreply.github.com>
2018-10-05 15:29:29 -0300

    EZL-1038: acerto de mesclagem na migration

--- task/migrations/0116_add_ecmtask.py
+++ task/migrations/0116_add_ecmtask.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('task', '0114_update_delegation_date'),
+        ('task', '0115_task_performance_place'),
     ]
 
     operations = [
