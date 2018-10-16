323a89aa20a2e35bbb1b3697065a12284c28f391
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2018-10-16 17:54:08 -0300

    EZL-888 - Acerto de campos obrigat?rios no endpoint de task

--- task/serializers.py
+++ task/serializers.py
@@ -1,5 +1,6 @@
 from .models import TypeTask, Task, Ecm, TypeTaskMain
 from rest_framework import serializers
+from core.models import Person
 from core.serializers import OfficeDefault, CreateUserDefault, CreateUserSerializerMixin, OfficeSerializerMixin
 
 
@@ -23,6 +24,10 @@ class TaskSerializer(serializers.ModelSerializer, CreateUserSerializerMixin, Off
 
 class TaskCreateSerializer(serializers.ModelSerializer,
                            CreateUserSerializerMixin, OfficeSerializerMixin):
+
+    person_asked_by = serializers.PrimaryKeyRelatedField(allow_null=False, label='Solicitante',
+                                                         queryset=Person.objects.all(), required=True)
+
     class Meta:
         model = Task
         exclude = ('create_date', 'alter_date', 'system_prefix', 'is_active',
