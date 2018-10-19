cfc927492b001ea6c14a35b5241e65795e1a5f60
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2018-10-19 15:02:51 -0300

    Ajustaodo ordenacao do chat

--- chat/views_api.py
+++ chat/views_api.py
@@ -12,7 +12,7 @@ class ChatViewSet(viewsets.ModelViewSet):
 
 
 class UnreadMessageViewSet(viewsets.ModelViewSet):
-    queryset = UnreadMessage.objects.all()
+    queryset = UnreadMessage.objects.all().order_by('message__chat__tasks_company_chat__final_deadline_date', '-message_id')
     serializer_class = UnreadMessageSerializer
     filter_backends = (DjangoFilterBackend, )
     filter_fields = ('user_by_message__user_by_chat_id', 'message__message', 'message__chat__company_id')
