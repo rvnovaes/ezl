7cd6f95195a3cec21640f702d0566d8db2a2cac1
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2018-12-27 16:31:09 -0200

    ajustando checkout

--- billing/static/billing/js/checkout.js
+++ billing/static/billing/js/checkout.js
@@ -1,14 +1,46 @@
 class Checkout {
 	constructor() {
 		this._elModalCheckout = $('#modal-billing-checkout');
+		this._elInputName = $('#input-name');		
+		this._elInputEmail = $('#input-email');
+		this._elInputCPF = $('#input-cpf');
+		this._elInputBirth = $('#input-birth');
+		this._elInputPhoneNumber = $('#input-phone-number');
+		this._elInputBrand = $('[name=input-card-brand]');
 		this._elInputCardNumber = $('#input-card-number');
 		this._elInputCardExpirationDate = $('#input-card-expiration-date');
 		this._elInputCardCVVCode = $('#input-card-cvv-code'); 
-		this._elInputCardName = $('#input-card-name');		
+		this._elInputZipcode = $('#input-address-zipcode');
+		this._elInputStreet = $('#input-address-street');
+		this._elInputAddressNumber = $('#input-address-number');
+		this._elInputNeighborhood = $('#input-address-neighborhood');
+		this._elInputComplement = $('#input-address-complement');
+		this._elInputCity = $('#input-address-city');
+		this._elState = $('#input-address-state');
 		this._elBtnPay = $('#btn-pay');
 		this.onClickBtnPay();
 		this.onClickBrand();
 
+	}
+
+	get name() {
+		return this._elInputCardName.val();
+	}
+
+	get email() {
+		return this._elInputEmail.val();
+	}	
+
+	get cpf() {
+		return this._elInputCPF.val();
+	}
+
+	get birth() {
+		return this._elInputBirth.val();
+	}
+
+	get phoneNumber() {
+		return this._elInputPhoneNumber.val();
 	}
 
 	get cardNumber() {
@@ -21,11 +53,7 @@ class Checkout {
 
 	get cardCVVCode() {
 		return this._elInputCardCVVCode.val();		
-	}
-
-	get cardName() {
-		return this._elInputCardName.val();
-	}
+	}	
 
 	openCheckout() {
 		this._elModalCheckout.modal('show')
