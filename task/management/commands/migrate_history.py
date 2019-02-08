9b4cfd1270927463366d9582e401d34f3d600b7f
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2019-02-08 11:08:01 -0200

    reduzindo o numero do grupo de inser??o e limpando a memoria de consultas

--- task/management/commands/migrate_history.py
+++ task/management/commands/migrate_history.py
@@ -1,3 +1,4 @@
+from django.db import reset_queries
 from django.core.management.base import BaseCommand
 from task.models import Task, TaskHistory, HistoricalTask
 import logging
@@ -18,18 +19,19 @@ def batch_qs(qs, batch_size=1000):
 
 
 class Command(BaseCommand):
-    help = ('Migra os historicos de TaskHistory para HistoricalTask')
+    help = 'Migra os historicos de TaskHistory para HistoricalTask'
 
-    def handle(self, *args, **options):        
+    def handle(self, *args, **options):
         historys = []
         history_qs = TaskHistory.objects.all()
-        for start, end, total, qs in batch_qs(history_qs, batch_size=1000):
+        for start, end, total, qs in batch_qs(history_qs, batch_size=500):
             LOGGER.info("Now processing %s - %s of %s" % (start + 1, end, total))            
             for task_history in qs:        
                 history = HistoricalTask(
-                    id=task_history.task.pk, history_date=task_history.create_date, history_office=task_history.task.office,
-                    task_status=task_history.status, history_user=task_history.create_user,
-                    history_notes=task_history.notes)
+                    id=task_history.task.pk, history_date=task_history.create_date,
+                    history_office_id=task_history.task.office.pk, task_status=task_history.status,
+                    history_user_id=task_history.create_user.pk, history_notes=task_history.notes)
                 historys.append(history)            
-            HistoricalTask.objects.bulk_create(historys, batch_size=1000)
+            HistoricalTask.objects.bulk_create(historys, batch_size=500)
             historys.clear()
+            reset_queries()
