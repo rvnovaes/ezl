035681aa719c173560b029d2a69096288148e7f4
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2019-02-13 14:44:27 -0200

    Ajustado problema da migration

--- core/migrations/0130_clean_cpf_cnpj_office.py
+++ core/migrations/0130_clean_cpf_cnpj_office.py
@@ -5,15 +5,27 @@ from __future__ import unicode_literals
 from django.db import migrations
 from core.signals import create_office_custom_settings
 from django.apps import apps
+import re
+import logging
+from django.db import transaction
+
+LOGGER = logging.getLogger('0130_clean_cpf_cnpj_office')
+
+def clear_cpf_cnpj(cpf_cnpj):
+    return re.sub(r'[^0-9]', '', cpf_cnpj)
 
 
 def clean_cpf_cnpj_office(apps, schema_editor):
     Office = apps.get_model('core', 'office')
     for office in Office.objects.all():
-        try:
-            office.save()
-        except: 
-            pass
+        if office.cpf_cnpj:
+            office.cpf_cnpj = clear_cpf_cnpj(office.cpf_cnpj)
+            with transaction.atomic():
+                try:
+                    office.save()
+                except Exception as e:
+                    LOGGER.info('ERRO AO ATUALIZAR CPF/CNPJ DE {} - {}'.format(
+                        office.legal_name, office.cpf_cnpj))            
 
 
 class Migration(migrations.Migration):
