53069c9104f94069cdde43a5b26173a30d92a0d2
Tiago de Souza Lima Gomes <tiagoslg@gmail.com>
2019-04-01 09:48:43 -0300

    correcao de fixture e migration para setar valor padrao diferente parao MTA

--- manager/migrations/0010_load_template_fixture.py
+++ manager/migrations/0010_load_template_fixture.py
@@ -6,11 +6,20 @@ from django.db import migrations
 import logging
 from django.core.management import call_command
 
-logger = logging.getLogger('0005_migrate_custom_settings')
+logger = logging.getLogger('0010_load_template_fixture')
 
 
 def load_fixture(apps, schema_editor):
     call_command('loaddata', 'template', app_label='manager')
+
+
+def update_mta(apps, schema_editor):
+    from manager.enums import TemplateKeys
+    from manager.utils import update_template_value
+    from etl.utils import get_default_office
+    office = get_default_office()
+    template_obj = office.get_template_value_obj(TemplateKeys.DUPLICATE_PROCESS.name)
+    update_template_value(template_obj, str(2))
 
 
 class Migration(migrations.Migration):
@@ -21,4 +30,5 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(load_fixture),
+        migrations.RunPython(update_mta),
     ]
